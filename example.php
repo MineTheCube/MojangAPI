75e919d742aefb62c3009012ea24324257c06d49
MineTheCube <MineTheCube@users.noreply.github.com>
2016-05-20 00:38:40 +0200

    Init commit

---
+++ example.php
@@ -0,0 +1,33 @@
+<?php
+
+// Require API
+// ======================
+
+require 'mojang-api.php';
+
+
+// Fetch stuff
+// ======================
+
+$username = 'MTC';
+
+$uuid = MojangAPI::getUuid($username);
+
+$full_uuid = MojangAPI::formatUuid($uuid);
+
+$history = MojangAPI::getNameHistory($uuid);
+
+$img = '<img src="' . MojangAPI::embedImage(MojangAPI::getPlayerHead($uuid)) . '" alt="Head of ' . $username . '">';
+
+
+// And print it
+// ======================
+
+echo '<h2>' . $username . '</h2>';
+
+echo 'UUID: <b>' . $full_uuid . '</b><br>';
+
+echo 'First username: <b>' . reset($history)['name'] . '</b><br>';
+
+echo 'Skin:<br>';
+echo $img;
